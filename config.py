f5ef3cdfe198657906d7a0bdc9c4d54c934dff2e
sammirsharifi <72696817+sammirsharifi@users.noreply.github.com>
2025-09-16 22:11:08 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-ADMIN_BOT_TOKEN = ""
-USER_BOT_TOKEN=""
-ADMIN_IDS = []   # Replace with your Telegram user ID
+import os
+
+
+ADMIN_BOT_TOKEN = os.getenv("ADMIN_BOT_TOKEN") or None
+USER_BOT_TOKEN=os.getenv("USER_BOT_TOKEN") or None
+ADMIN_IDS = os.getenv("ADMIN_IDS") or None   # Replace with your Telegram user ID
 USER_BOT_USERNAME = "Piki_Food_bot"
 DB_NAME = "foodbot.db"
